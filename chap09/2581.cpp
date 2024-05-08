9018724287ac74649cfb145adffe39da073e44f9
pcadl0196 <pcadl0196@gmail.com>
2024-05-08 22:32:58 +0900

    240508_1

---
+++ chap09/2581.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // 1. m 부터 n까지 for 문을 돌리고 1과 자기 자신만을 나누었을때 0이 되는 수만 카운트 함
+    // 2. 카운트가 2개 였을때만 벡터 배열에 푸시함
+    // 3. 벡터 배열안에 아무것도 들어있지 않으면 -1 출력
+    // 4. 그렇지 않으면 sum 과 최솟값 출력
+
+    int m, n;
+    cin >> m >> n;
+
+    vector<int> vec;
+    return 0;
+}
