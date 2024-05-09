e1dfe8a2f56431319117dc955a62bc6046142f01
pcadl0196 <pcadl0196@gmail.com>
2024-05-09 21:13:28 +0900

    240509

--- chap09/2581.cpp
+++ chap09/2581.cpp
@@ -14,5 +14,38 @@ int main()
     cin >> m >> n;
 
     vector<int> vec;
+
+    for(int i = m; i <= n; i++)
+    {
+        int cnt = 0;
+        for(int j = 1; j <= i; j++)
+        {
+            if(i%j == 0)
+            {
+                cnt++;
+            }
+        }
+
+        if(cnt == 2)
+        {
+            vec.push_back(i);
+        }
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < vec.size(); i++)
+    {
+        sum += vec[i];
+    }
+
+    if(vec.size() == 0)
+    {
+        cout << "-1";
+    }
+    else
+    {
+        cout << sum << "\n" << vec[0];
+    } 
     return 0;
 }
